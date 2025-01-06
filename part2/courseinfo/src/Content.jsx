1e324a820ea87c8d62669537e8fa4b112a9c72dd
Jarkko Tuikka <jarkko.tuikka@gmail.com>
2025-01-06 17:17:49 +0200

    2.1

---
+++ part2/courseinfo/src/Content.jsx
@@ -0,0 +1,11 @@
+import Part from './Part'
+
+const Content = (props) => {
+    return (
+        <div>
+            {props.parts.map(part => <Part part={part} /> )}
+        </div>
+    )
+}
+
+export default Content
