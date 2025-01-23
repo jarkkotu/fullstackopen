6337b296a7c1a4d584ecec1f294becd3b7db0844
Jarkko Tuikka <jarkko.tuikka@gmail.com>
2025-01-23 23:57:40 +0200

    4.8-4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,27 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const body = request.body
-
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
   })
 
-  blog.save()
-    .then(saved => {
-      response.json(saved)
-    })
-    .catch(error => next(error))
+  const saved = await blog.save()
+  response.status(201).json(saved)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+  }
+
+  const updated = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true, context: 'query' })
+  response.json(updated)
 })
 
 module.exports = blogsRouter
---
+++ part4/tests/api.test.js
@@ -0,0 +1,126 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const helper = require('./test_helper')
+const mongoose = require('mongoose')
+
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect(response => {
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+})
+
+test('_id is transformed to id', async () => {
+  const response = await api.get('/api/blogs')
+  assert.ok(response.body[0].id)
+  assert.ok(!response.body[0]._id)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Test blog',
+    author: 'Test author',
+    url: 'http://test.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+    .expect(response => assert.strictEqual(response.body.title, newBlog.title))
+    .expect(response => assert.strictEqual(response.body.author, newBlog.author))
+    .expect(response => assert.strictEqual(response.body.url, newBlog.url))
+    .expect(response => assert.strictEqual(response.body.likes, newBlog.likes))
+    .expect(response => assert.ok(response.body.id))
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+})
+
+test('invalid blog is not added', async () => {
+  const invalidBlogs = [
+    {
+      likes: 0
+    },
+    {
+      title: 'Test blog',
+    },
+    {
+      title: 'Test blog',
+      author: 'Test author'
+    },
+    {
+      title: 'Test blog',
+      url: 'http://test.com'
+    }]
+
+  for (let invalidBlog of invalidBlogs) {
+    await api
+      .post('/api/blogs')
+      .send(invalidBlog)
+      .expect(400)
+  }
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('blog without likes defaults to 0', async () => {
+  const newBlog = {
+    title: 'Test blog',
+    author: 'Test author',
+    url: 'http://test.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+    .expect(response => assert.strictEqual(response.body.likes, 0))
+})
+
+test('blog can be removed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+})
+
+test('blog can be updated', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToUpdate = blogsAtStart[0]
+
+  blogToUpdate.likes = 123
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(blogToUpdate)
+    .expect(200)
+    .expect(response => assert.strictEqual(response.body.likes, blogToUpdate.likes))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
